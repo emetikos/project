11ede85e72d948496d5210d1484abdc4807efe8e
emetikos <spirosvelos@yahoo.com>
2022-01-10 10:36:32 +0000

    test mail

---
+++ jikMailer/resources/views/emails/testMail.blade.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jobSeeker - Mail</title>
+
+    <style>
+        @media (max-width: 480px) {
+            .greeting-header {
+                font-size: .5rem;
+            }
+        }
+    </style>
+
+</head>
+<body style="background-color: #F0F0F0; max-width: 60rem;">
+<div class="container" style="margin: auto; padding: 0 10px;">
+
+    <div class="header" style="background-color: #554595; color: white; font-size: 1.125rem; font-weight: bold; text-align: center; padding: 0 10px;"><span class="span-header" style="font-size: 2.25rem;">jobsinkent.com</span> Kent's largest jobsite</div>
+
+
+
+    <div class="body col">
+        <div class="greeting" style="display: flex; align-content: center; padding-left: 2rem;">
+            <p class="greeting-header" style="font-size: 2rem;">Dear jobseeker,  let this year make your dreams true and let us show the best jobs in the market for you!</p>
+        </div>
+        <br> <br><br>
+
+        <div class="row mb-4" style="padding-left: 3rem;">
+            <div class="col"><p style="font-size: 1.125rem;">Check the most viewed jobs and the most popular industries</p></div>
+        </div>
+        <div class="row">
+            <div class="job col" style="padding-left: 5%; padding-right: 5%;">
+                <a href="https://www.jobsinkent.com/search?q=warehouse+operatives">
+                    <div class="context" style="padding-left: 5%; padding-right: 5%; width: auto;">
+                        <div class="job-title" style="color: black; font-weight: bold; font-size: 1.25rem; text-decoration: underline;">Warehouse Operative</div>
+                        <div class="job-location" style="color: black; font-weight: bold; font-size: 1.125rem;">Kent</div>
+                        <div class="job-salary" style="color: black; font-size: 1.125rem;">£8.91 - £15 based on location</div>
+                        <div class="job-vacancies" style="color: black; opacity: 70%;">vacancies: 170+</div>
+                    </div>
+                </a>
+            </div>
+            <div class="job col" style="padding-left: 5%; padding-right: 5%;">
+                <a href="https://www.jobsinkent.com/search?q=Forklift+Driver">
+                    <div class="context" style="padding-left: 5%; padding-right: 5%; width: auto;">
+                        <div class="job-title" style="color: black; font-weight: bold; font-size: 1.25rem; text-decoration: underline;">Forklift Driver</div>
+                        <div class="job-location" style="color: black; font-weight: bold; font-size: 1.125rem;">Kent</div>
+                        <div class="job-salary" style="color: black; font-size: 1.125rem;">£10.50 - £11.66 based on location</div>
+                        <div class="job-vacancies" style="color: black; opacity: 70%;">vacancies: 40+</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="job col" style="padding-left: 5%; padding-right: 5%;">
+                <a href="https://www.jobsinkent.com/search?q=Planning+coordinators">
+                    <div class="context" style="padding-left: 5%; padding-right: 5%; width: auto;">
+                        <div class="job-title" style="color: black; font-weight: bold; font-size: 1.25rem; text-decoration: underline;">Planning coordinators </div>
+                        <div class="job-location" style="color: black; font-weight: bold; font-size: 1.125rem;">Maidstone, Kent</div>
+                        <div class="job-salary" style="color: black; font-size: 1.125rem;">Doe</div>
+                        <div class="job-vacancies" style="color: black; opacity: 70%;">vacancies: 1</div>
+                    </div>
+                </a>
+            </div>
+            <div class="job col" style="padding-left: 5%; padding-right: 5%;">
+                <a href="https://www.jobsinkent.com/search?q=Commercial+Account+Handler">
+                    <div class="context" style="padding-left: 5%; padding-right: 5%; width: auto;">
+                        <div class="job-title" style="color: black; font-weight: bold; font-size: 1.25rem; text-decoration: underline;">Commercial Account Handler </div>
+                        <div class="job-location" style="color: black; font-weight: bold; font-size: 1.125rem;">Kent</div>
+                        <div class="job-salary" style="color: black; font-size: 1.125rem;">£20000 - £400000</div>
+                        <div class="job-vacancies" style="color: black; opacity: 70%;">vacancies: 10+</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="job col" style="padding-left: 5%; padding-right: 5%;">
+                <a href="https://www.jobsinkent.com/search?q=Business+Development+Executive">
+                    <div class="context" style="padding-left: 5%; padding-right: 5%; width: auto;">
+                        <div class="job-title" style="color: black; font-weight: bold; font-size: 1.25rem; text-decoration: underline;">Business Development Executive </div>
+                        <div class="job-location" style="color: black; font-weight: bold; font-size: 1.125rem;">Ramsgate, Kent</div>
+                        <div class="job-salary" style="color: black; font-size: 1.125rem;">£20000 - £300000</div>
+                        <div class="job-vacancies" style="color: black; opacity: 70%;">vacancies: 4</div>
+                    </div>
+                </a>
+            </div>
+            <div class="job col" style="padding-left: 5%; padding-right: 5%;">
+                <a href="https://www.jobsinkent.com/search?q=Finance+Administrator">
+                    <div class="context" style="padding-left: 5%; padding-right: 5%; width: auto;">
+                        <div class="job-title" style="color: black; font-weight: bold; font-size: 1.25rem; text-decoration: underline;">Finance Administrator</div>
+                        <div class="job-location" style="color: black; font-weight: bold; font-size: 1.125rem;">Maidstone,Kent</div>
+                        <div class="job-salary" style="color: black; font-size: 1.125rem;">Up to £10ph</div>
+                        <div class="job-vacancies" style="color: black; opacity: 70%;">vacancies: 10+</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <br><br>
+
+        <div class="row mb-4" style="padding-left: 3rem;">
+            <div class="col"><p style="font-size: 1.125rem;">Our most popular Industries holds the best opportunities  for you !</p></div>
+        </div>
+
+        <div class="wrapper" style="display: flex; width: 100%; flex-wrap: wrap; text-align: center; justify-content: space-evenly;">
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=25"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Administration</div></a></div>
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=1"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Accountancy</div></a></div>
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=38"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Media & Design</div></a></div>
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=9"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Education</div></a></div>
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=50"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Cleaning</div></a></div>
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=39"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Landscaping</div></a></div>
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=21"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Management</div></a></div>
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=29"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Retail</div></a></div>
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=2"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Agriculture</div></a></div>
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=40"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Hospitality</div></a></div>
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=16"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Manufacturing</div></a></div>
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=33"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Part-Time</div></a></div>
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=22"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Marketing</div></a></div>
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=17"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Insurance</div></a></div>
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=34"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Driving</div></a></div>
+            <div class="item" style="min-width: 14rem;"><a href="https://www.jobsinkent.com/search?i=36"><div class="industry" style="font-size: 1.25rem; color: #554595; margin-bottom: .875rem; margin-left: .875rem; margin-right: .875rem; border-radius: .3rem; border: 1px solid #554595; box-shadow: 0 4px 8px 0 rgba(97, 69, 153, 0.2), 0 6px 20px 0 rgba(97, 69, 153, 0.19);">Warehouse</div></a></div>
+        </div>
+
+
+    </div>
+    <div class="spa" style="height: .4rem"></div>
+
+
+
+    <div class="end" style="background-color: #554595; color: white; text-align: center; padding-top: .5rem; padding-bottom: .5rem;">
+        <p class="para-end" style="text-align: center; font-size: 1rem;">This email was sent to "jobseeker@mail"  from JIK Software Ltd, trading as <a href="https://www.jobsinkent.com/" style="color: white; font-weight: bold;  text-decoration: underline;">jobsinkent.com</a> . </p>
+        <p class="para-end" style="text-align: center; font-size: 1rem;">Whilst reasonable precaution has been taken to minimise risks, we cannot accept liability for any damage which you sustain as a result of software viruses. You should therefore carry out your own virus checks before opening any attachment.</p>
+        <p class="para-end" style="text-align: center; font-size: 1rem;"><a href="https://www.jobsinkent.com/jobseekers/profile" style="color: white; text-decoration: underline; font-weight: bold;">Click here</a> to manage your email preferences. </p>
+        <p class="para-end" style="text-align: center; font-size: 1rem;">© 2022 JIK Software Ltd </p>
+    </div>
+
+</div>
+
+</body>
+</html>
